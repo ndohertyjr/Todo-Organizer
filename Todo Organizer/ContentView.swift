a2a2cd3f14461017deee6c0a36df04bacc6214c3
Neil Doherty <neildohertyjr@gmail.com>
2022-07-29 10:27:41 -0500

    Initial Commit

---
+++ Todo Organizer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Todo Organizer
+//
+//  Created by user220431 on 7/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Todo Organizer/Todo_OrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_OrganizerApp.swift
+//  Todo Organizer
+//
+//  Created by user220431 on 7/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Todo_OrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
